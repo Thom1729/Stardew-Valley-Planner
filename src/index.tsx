d4a560c86c40f14d4756713d0b708b0abd12429f
Thomas Smith <thom@thomthesmith.com>
2023-03-31 12:21:46 -0400

    Update deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './components/App';
 
@@ -9,9 +10,10 @@ declare module 'react' {
     }
 }
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
