45a8b79c27ab50b6e50320c3a1024d67bd2f0cbb
Thomas Smith <thom@thomthesmith.com>
2024-04-02 11:11:14 -0400

    Factor out planting controls

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,11 +1,8 @@
 import './Table.scss';
 
-import { useCallback } from 'react';
 import type { FC } from 'react';
 
-import { Button, NumberInput } from './components';
-import { StateUpdater, useArraySubstates, useObjectSubstates } from './substate';
-import { CropSelector } from './CropSelector';
+import { StateUpdater, } from './substate';
 
 import { Star } from './svg';
 import { range, pairs } from '../util';
@@ -13,8 +10,8 @@ import { range, pairs } from '../util';
 import type { Planting, Options } from './state';
 
 import { getEvents, type Event } from './calc';
-import { FertilizerSelector } from './FertilizerSelector';
 import { Cell } from './DisplayHelpers';
+import { PlantingsControls } from './PlantingControls';
 
 type AggregateFunction = (event: Event) => number;
 
@@ -194,92 +191,6 @@ export const Table: FC<{
       )}
     </Cell>
   </div>;
-};
-
-const PlantingsControls: React.FC<{
-  value: readonly Planting[],
-  onChange: StateUpdater<readonly Planting[]>,
-}> = ({
-  value,
-  onChange,
-}) => {
-  const substates = useArraySubstates(onChange);
-
-  const newPlanting = useCallback(
-    () => substates.append(plantings => ({
-      id: 1 + Math.max(0, ...plantings.map(p => p.id)),
-      plantDate: 1,
-      quantity: 1,
-      cropId: 24,
-      fertilizer: null,
-    })),
-    [substates],
-  );
-
-  return <Cell group>
-    <Cell group
-      className='Planting-Table-Headers'
-      row='header'
-    >
-      <Cell column='planting-quantity'>Qty</Cell>
-      <Cell column='planting-crop'>Crop</Cell>
-      <Cell column='planting-start'>Date</Cell>
-      <Cell column='planting-fertilizer'>Fertilizer</Cell>
-    </Cell>
-
-    {value.map((planting, index) =>
-      <Cell group
-        key={`planting-${planting.id}`}
-        row={`planting ${index+1}`}
-      >
-        <PlantingControls
-          planting={planting}
-          plantingChanged={substates.set(index)}
-          deletePlanting={value.length > 1 ? substates.delete(index) : undefined}
-          move={substates.moveBy(index)}
-        />
-      </Cell>
-    )}
-
-    <Cell group row='planting-new' column='left-start / left-end'>
-      <Button style={{ justifySelf: 'end' }} onClick={newPlanting}>New</Button>
-    </Cell>
-  </Cell>;
-};
-
-const PlantingControls: React.FC<{
-  planting: Planting,
-  plantingChanged: StateUpdater<Planting>,
-  deletePlanting: (() => void) | undefined,
-  move: (diff: number) => void,
-}> = ({
-  planting,
-  plantingChanged,
-  deletePlanting,
-  move,
-}) => {
-  const substates = useObjectSubstates(plantingChanged);
-  return <>
-    <Cell group column='planting-delete'>
-      <Button onClick={deletePlanting} disabled={deletePlanting === undefined}>Delete</Button>
-    </Cell>
-    <Cell group column='planting-quantity'>
-      <NumberInput value={planting.quantity} onChange={substates.set('quantity')} min={0} step={1} />
-    </Cell>
-    <Cell group column='planting-crop'>
-      <CropSelector value={planting.cropId} onChange={substates.set('cropId')} />
-    </Cell>
-    <Cell group column='planting-start'>
-      <NumberInput value={planting.plantDate} onChange={substates.set('plantDate')} min={1} max={28} step={1} />
-    </Cell>
-    <Cell group column='planting-fertilizer'>
-      <FertilizerSelector value={planting.fertilizer} onChange={substates.set('fertilizer')} />
-    </Cell>
-    <Cell column="planting-move">
-      {/*<Button onClick={() => { move(-1); }}>↑</Button>*/}
-      <Button onClick={() => { move(1); }}>↓</Button>
-    </Cell>
-  </>;
 };
 
 const gFormat = new Intl.NumberFormat('en-us', {
